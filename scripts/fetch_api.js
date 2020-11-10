601de3e0ba6591103f573c8dfa706d11f8c1e283
Marc Rivers <mrivers2133@outlook.com>
2020-11-09 22:02:32 -0500

    Began implementing basic fetch functionality

--- scripts/fetch_api.js
+++ scripts/fetch_api.js
@@ -2,32 +2,48 @@
 // store in a JSON file?
 // how best to otuput the JSON? in a .txt?
 
-const fetch = require("node-fetch");  // FOR TESTING ONLY! REMOVE THIS FOR PRODUCTION CODE!
+// const fetch = require('node-fetch'); // FOR TESTING ONLY! REMOVE THIS FOR PRODUCTION CODE!
+const output = document.querySelector('#countries');
+const displayData = document.querySelector('#results');
+let apiData;
 
 // ------------------------------------------
 //  FETCH FUNCTIONS
 // ------------------------------------------
 
-function fetchData(url) {
-    return fetch(url)
-        .then(checkStatus)
-        // .then(res => res.json())
-        // .catch(error => console.log('Looks like there was a problem.', error))
+async function fetchData(url) {
+  const data = await fetch(url);
+  // .then(checkStatus)
+  // .then((res) => res.json())
+  // .then((data) => console.log(data))
+  // .catch((error) => console.log('Looks like there was a problem.', error));
+  return await data.json();
 }
 
-Promise.all([
-    fetchData('https://api.openaq.org/v1/cities'),
-])
-
-// ------------------------------------------
-//  HELPER FUNCTIONS
-// ------------------------------------------
-
-function checkStatus (response) {
-    if (response.ok) {
-        // return Promise.resolve(response);
-        console.log(response)
-    } else {
-        return Promise.reject(new Error(response.statusText));
-    }
+function handleSelect(evt) {
+  console.log(evt.target.value);
+  console.log(apiData);
+  const myChoice = apiData.find((obj) => evt.target.value === obj.name);
+  console.log(myChoice);
+  // Change innerHTML of comparison div in this function
 }
+
+fetchData('https://api.openaq.org/v1/cities?limit=200').then((res) => {
+  apiData = res.results;
+  let countryCode = '';
+  res.results.forEach((countryObject) => {
+    console.log(countryObject);
+    countryCode = countryCode.concat(
+      ' ',
+      `<option value="${countryObject.name}">${countryObject.name}</option>`
+    );
+  });
+  output.innerHTML = countryCode;
+});
+// do all DOM manipulation inside this .then statement
+
+// ------------------------------------------
+//  EVENT LISTENERS
+// ------------------------------------------
+
+output.addEventListener('change', (evt) => handleSelect(evt));
